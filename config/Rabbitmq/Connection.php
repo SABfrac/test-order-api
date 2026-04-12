e0d19867fc7bfb361be9867f1ecf7b2b399fbd4f
SABfrac <nh846202@gmail.com>
2026-04-12 04:29:20 +0300

    fix reconnect rabbitMQ

--- config/Rabbitmq/Connection.php
+++ config/Rabbitmq/Connection.php
@@ -25,6 +25,11 @@ final class Connection
 
     public function channel(): AMQPChannel
     {
+        if ($this->ch !== null && (!$this->conn->isConnected() || !$this->ch->is_open())) {
+            $this->ch = null;
+            $this->conn = null;
+        }
+
         if ($this->ch === null) {
             $this->conn ??= new AMQPStreamConnection($this->host, $this->port, $this->user, $this->pass, $this->vhost);
             $this->ch = $this->conn->channel();
